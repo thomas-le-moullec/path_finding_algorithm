d9bf4c91b1e8a67b3b7ec94baefecef1fcd14bc7
chabot_t <thomas.chabot@epitech.eu>
2016-04-23 18:16:13 +0200

    rect .h

--- bonus/includes/lem_in.h
+++ bonus/includes/lem_in.h
@@ -5,7 +5,7 @@
 ** Login   <le-mou_t@epitech.net>
 ** 
 ** Started on  Sun Apr 17 18:54:16 2016 Thomas LE MOULLEC
-** Last update Wed Apr 20 11:23:46 2016 leo LE DIOURON
+** Last update Sat Apr 23 18:15:41 2016 Thomas CHABOT
 */
 
 #ifndef LEMIN_H_
@@ -23,79 +23,61 @@ int             fill_links(t_data *);
 int             fill_tubes(t_data *);
 int             loop_tubes(t_data *, int *, int, int);
 int             count_line(t_data *, int);
-
 int             fill_id_pipe(t_data *, int);
 int             my_cut_id(t_data *, int);
 int             my_cut_other(t_data *, int);
 int             my_strcmp_colon(char *, char *);
 int             my_strcmp_other(char *, char *);
-
 void            free_nodes(t_data *);
-
 int             valid_node(char *);
-
 char            *copy_line(t_data *, int *);
 int             find_type(t_data *, char *);
 int             check_line(t_data *, int *);
 int             check_buffer(t_data *);
-
 int             init_infos(t_data *);
 int             init_functions(t_data *);
 int             init_parser(t_data *);
 int             init_nodes(t_data *);
 void            init_id_pipe(t_data *, int);
-
 int             take_parser(t_data *);
 int             end_parser(t_data *);
 int             parser(int, t_data *);
-
 int             fill_nbr_ants(t_data *, char *);
 int             fill_nbr_nodes(t_data *);
-
 char            *find_name(char *);
 int             double_com_fct(int *, char *, t_data *);
 int             my_start(int *, t_data *);
 int             my_end(int *, t_data *);
-
 char            *links_cpy(int *, char *);
 int             static_ptr(t_data *, int *);
 int             links_cmp(t_data *, char *, char *, char *);
 char            *other_cpy(int *, char *, int);
-
 int             simple_com_fct(int *, char *, t_data *);
 int             links_fct(int *, char *, t_data *);
 int             nodes_fct(int *, char *, t_data *);
-
 int		error_default(char *);
-
 int             error_nbr_nodes();
 int             error_nbr_ants();
-
 int             error_malloc();
 int             error_copying(t_data *);
 int             error_reading();
 int             error_args();
-
 int             error_link(char *, t_data *);
 int             error_correspondance(char *, t_data *);
 int             error_node(t_data *, char *);
 int             error_simple_com(t_data *, char *);
 int             error_type(t_data *, char *);
-
 int		algo_loop(t_data *, int *, int, int *);
 int		algo(t_data *, int, int);
 int		find_best_path(t_data *, int);
 int		flag_nodes_path(t_data *, int *, int);
 int		check_history(int *, int);
-
 int		algo_ant(t_data *);
 int		move_ants(t_data *, int);
 int		fill_ant(t_data *);
-
 int             creat_list(t_data *);
 void            delete_elem(t_path **);
 int             cpy_elem(t_path **, int);
-
 int                     main_loop(t_data *);
 int                     init_graph(t_data *);
 int                     graphic(t_data *);
@@ -107,14 +89,11 @@ int                     init_win(t_data *);
 int                     draw_nodes(t_data *);
 int                     draw_links(t_data *);
 int                     move_ant_graph(t_data *);
-
 void                    noise_color_full(t_bunny_pixelarray  *);
-
 void                    tekpixel(t_bunny_pixelarray *,\
 				 t_bunny_position, unsigned int);
 void                    tekpixel2(t_bunny_pixelarray *,\
 				  t_bunny_position, unsigned int);
-
 int                     tekline(t_bunny_pixelarray *,\
 			       t_bunny_position *, t_data *, unsigned int);
 void                    tekline_vertical(t_bunny_pixelarray *,\
@@ -122,28 +101,19 @@ void                    tekline_vertical(t_bunny_pixelarray *,\
 void                    tekline_swap(t_bunny_pixelarray *,\
 				     t_bunny_position *, unsigned int);
 t_bunny_position        *swap(t_bunny_position *);
-
 void                    tekcircle(t_bunny_pixelarray *,\
 				  t_bunny_position, unsigned int, int);
-
 char		*my_strcpy_ptr(char *, char *, int);
 char            *my_strcpy_colon(char *, char *);
-
 int             my_strlen(char *);
-
 void		my_putchar(char, int);
 void		my_putstr(char *, int);
 void            my_putstr_buffer(char *);
-
 int             char_epur(char, char);
 char            *my_epur_str(char *);
-
 int		my_get_nbr(char *);
-
 void            my_put_nbr(int, int);
-
 void            my_free(void *);
-
 int		my_strcmp(char *, char *);
 
 #endif
--- includes/lem_in.h
+++ includes/lem_in.h
@@ -5,7 +5,7 @@
 ** Login   <le-mou_t@epitech.net>
 ** 
 ** Started on  Sun Apr 17 18:54:16 2016 Thomas LE MOULLEC
-** Last update Wed Apr 20 10:46:22 2016 Thomas CHABOT
+** Last update Sat Apr 23 18:14:19 2016 Thomas CHABOT
 */
 
 #ifndef LEMIN_H_
@@ -20,97 +20,72 @@ int             fill_links(t_data *);
 int             fill_tubes(t_data *);
 int             loop_tubes(t_data *, int *, int, int);
 int             count_line(t_data *, int);
-
 int             fill_id_pipe(t_data *, int);
 int             my_cut_id(t_data *, int);
 int             my_cut_other(t_data *, int);
 int             my_strcmp_colon(char *, char *);
 int             my_strcmp_other(char *, char *);
-
 void            free_nodes(t_data *);
-
 int             valid_node(char *);
-
 char            *copy_line(t_data *, int *);
 int             find_type(t_data *, char *);
 int             check_line(t_data *, int *);
 int             check_buffer(t_data *);
-
 int             init_infos(t_data *);
 int             init_functions(t_data *);
 int             init_parser(t_data *);
 int             init_nodes(t_data *);
 void            init_id_pipe(t_data *, int);
-
 int             take_parser(t_data *);
 int             end_parser(t_data *);
 int             parser(int, t_data *);
-
 int             fill_nbr_ants(t_data *, char *);
 int             fill_nbr_nodes(t_data *);
-
 char            *find_name(char *);
 int             double_com_fct(int *, char *, t_data *);
 int             my_start(int *, t_data *);
 int             my_end(int *, t_data *);
-
 char            *links_cpy(int *, char *);
 int             static_ptr(t_data *, int *);
 int             links_cmp(t_data *, char *, char *, char *);
 char            *other_cpy(int *, char *, int);
-
 int             simple_com_fct(int *, char *, t_data *);
 int             links_fct(int *, char *, t_data *);
 int             nodes_fct(int *, char *, t_data *);
-
 int		error_default(char *);
-
 int             error_nbr_nodes();
 int             error_nbr_ants();
-
 int             error_malloc();
 int             error_copying(t_data *);
 int             error_reading();
 int             error_args();
-
 int             error_link(char *, t_data *);
 int             error_correspondance(char *, t_data *);
 int             error_node(t_data *, char *);
 int             error_simple_com(t_data *, char *);
 int             error_type(t_data *, char *);
-
 int		algo_loop(t_data *, int *, int , int *);
 int		algo(t_data *, int, int);
 int		find_best_path(t_data *, int);
 int		flag_nodes_path(t_data *, int *, int);
 int		check_history(int *, int);
-
 int		algo_ant(t_data *);
 int		move_ants(t_data *, int);
 int		fill_ant(t_data *);
-
 int             creat_list(t_data *);
 void            delete_elem(t_path **);
 int             cpy_elem(t_path **, int);
-
 char		*my_strcpy_ptr(char *, char *, int);
 char            *my_strcpy_colon(char *, char *);
-
 int             my_strlen(char *);
-
 void		my_putchar(char, int);
 void		my_putstr(char *, int);
 void            my_putstr_buffer(char *);
-
 int             char_epur(char, char);
 char            *my_epur_str(char *);
-
 int		my_get_nbr(char *);
-
 void            my_put_nbr(int, int);
-
 void            my_free(void *);
-
 int		my_strcmp(char *, char *);
 
 #endif
